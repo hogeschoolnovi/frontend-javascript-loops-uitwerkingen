793d75b137095c9af5c4d88391fdf88421e33283
novaeeken <novaeeken@Novas-MacBook-Pro.local>
2021-03-22 23:04:03 +0100

    Add solutions

--- loops-and-arrays.js
+++ loops-and-arrays.js
@@ -12,6 +12,14 @@ const names = ["Henk", "Piet", "Fred", "Joop"];
 // Na jouw script zie je de aangepaste waardes:
 // console.log(names) geeft: ["Henkje", "Pietje", "Fredje", "Joopje"]
 
+console.log(names);
+
+for (let i = 0; i < names.length; i++) {
+  names[i] = names[i] + 'je';
+}
+
+console.log(names);
+
 
 // ==========================================
 // Opdracht 2
@@ -26,3 +34,15 @@ const numbers = [2, 4, 5, 29, 38];
 // console.log(numbers) geeft: [2, 4, 5, 29, 38];
 // Na jouw script zie je de aangepaste waardes:
 // console.log(numbers) geeft: [ 6, 12, 15, 87, 114 ];
+
+console.log(numbers);
+
+for (let i = 0; i < numbers.length; i++) {
+  numbers[i] = numbers[i] * 3;
+
+  // als je alleen de uitkomst logt (zoals hieronder) zou je de array niet veranderen, en dat wil je juist wel!
+  // Dit is dus niet de juiste aanpak:
+  // console.log(numbers[i] * 3);
+}
+
+console.log(numbers);
